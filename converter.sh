e0dafa587a8d1dc2652da5e6577384315976d010
ItsGamerik <53947944+ItsGamerik@users.noreply.github.com>
2023-02-05 20:19:20 +0100

    cleaning up files

--- converter.sh
+++ converter.sh
@@ -23,17 +23,16 @@ else
     echo "output file name: $outname"
 fi
 
+mkdir temp
+
 # video in audio und video splitten
-ffmpeg -i $vidname -map 0:0 -c copy video.mkv -map 0:1 audio0.aac -map 0:2 audio1.aac
+ffmpeg -i $vidname -map 0:0 -c copy ./temp/video.mkv -map 0:1 ./temp/audio0.aac -map 0:2 ./temp/audio1.aac
 
 
 # audio wieder kombinieren
-ffmpeg -i audio0.aac -i audio1.aac -filter_complex "[0:a][1:a]amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a]" -map "[a]" audio_combo.wav
+ffmpeg -i ./temp/audio0.aac -i ./temp/audio1.aac -filter_complex "[0:a][1:a]amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a]" -map "[a]" ./temp/audio_combo.wav
 
 # video und audio kombinieren
-ffmpeg -i video.mkv -i audio_combo.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 $outname
+ffmpeg -i ./temp/video.mkv -i ./temp/audio_combo.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 $outname
 
-
-rm *.aac
-rm *.wav
-rm video.mkv
+rm -rv ./temp
